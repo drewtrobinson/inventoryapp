1a794e650712153202fe91e9f68f11aa89162a2a
drewtrobinson <72264748+drewtrobinson@users.noreply.github.com>
2026-02-22 13:41:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,3 @@
-const CACHE_NAME = 'pantry-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './app.js',
-    './manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://unpkg.com/html5-qrcode',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
-];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
